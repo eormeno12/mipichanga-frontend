687e4f2b19e85fbe663518a248c5d715a9533545
Ernesto Ormeño <eormeno@Ernestos-MacBook-Pro.local>
2024-05-19 18:55:29 -0500

    :zap: Add nextjs proxy for production.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import { env } from './config';
+
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: `/users/:path*`,
+        destination: `${env.users_api}/users/:path*`,
+      },
+      {
+        source: `/auth/:path*`,
+        destination: `${env.users_api}/auth/:path*`,
+      },
+      {
+        source: `/matches/:path*`,
+        destination: `${env.matches_api}/matches/:path*`,
+      },
+      {
+        source: `/fields/:path*`,
+        destination: `${env.fields_api}/fields/:path*`,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
