cf4f05cf196680ead7f3516d0a1674f065f3d3ec
Ernesto Ormeño <eormeno@Ernestos-MacBook-Pro.local>
2024-05-19 20:01:26 -0500

    :recycle: Remove nextjs proxy.

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: `/users/:path*`,
-        destination: `${process.env.NEXT_PUBLIC_USERS_API}/users/:path*`,
-      },
-      {
-        source: `/auth/:path*`,
-        destination: `${process.env.NEXT_PUBLIC_USERS_API}/auth/:path*`,
-      },
-      {
-        source: `/matches/:path*`,
-        destination: `${process.env.NEXT_PUBLIC_MATCHES_API}/matches/:path*`,
-      },
-      {
-        source: `/fields/:path*`,
-        destination: `${process.env.NEXT_PUBLIC_FIELDS_API}/fields/:path*`,
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
